67dcc684eba40d2d5b3f872c3a3a784a190cb1fd
Ross Mawdsley <rossmawd@gmail.com>
2020-07-12 21:46:49 +0100

    added user migration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -4,11 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Data
 {
-    public class DataContext : DbContext
-    {
-        
-      public DataContext(DbContextOptions<DataContext> options) : base(options){}
+  public class DataContext : DbContext
+  {
 
-      public DbSet<Value> Values {get; set;}
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<Value> Values { get; set; }
+    public DbSet<User> Users { get; set; }
   }
 }
